3d1ca51c98f0bfe7b31c9620371f466ca1d5b43d
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-02-04 00:27:38 +0545

    completed tshirt design page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Link from "next/link";
 const Index = () => {
-  return <h1>ARTWARE.COM.NP</h1>;
+  return (
+    <div>
+      <h1>ARTWARE.COM.NP</h1>
+      <Link href={"/design/tshirt"}>Go to tshirt design page</Link>
+      <Link href={"/design/phonecover"}>Go to phone cover design page</Link>
+    </div>
+  );
 };
 
 export default Index;
