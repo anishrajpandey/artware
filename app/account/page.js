e0d835091369d88cf959f374583152696fda9bb9
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-02-11 17:05:37 +0545

    button

--- app/account/page.js
+++ app/account/page.js
@@ -1,36 +1,49 @@
 "use client";
 import React from "react";
 import style from "./account.module.css";
-
-import styles from "./account.module.css";
-
 const Index = () => {
   return (
     <main>
       <div className={style.signUp}>
         <h2>Sign Up for a new account </h2>
         <form action="/" className={style.signupForm}>
-          <div className={styles.nameSignup}>
+          <div className={style.nameSignup}>
             <label htmlFor="name">Your Name </label>
             <input type="text" />
           </div>
 
-          <div className={styles.phoneSignup}>
+          <div className={style.phoneSignup}>
             <label htmlFor="name">Your Phone Number </label>
             <input type="number" />
           </div>
-          <div className={styles.passwordSignup}>
+          <div className={style.passwordSignup}>
             <label htmlFor="name">Set Your Password </label>
             <input type="text" />
           </div>
-          <div className={styles.addressSignup}>
+          <div className={style.addressSignup}>
             <label htmlFor="name">Enter your address </label>
             {/* <input type="text" /> */}
-            <textarea name="address" id="address" cols="30" rows="10">
-              {" "}
-            </textarea>
+            <textarea
+              name="address"
+              id="address"
+              cols="30"
+              rows="10"
+            ></textarea>
           </div>
-          <button className={styles.submitButton}>Create Account </button>
+
+          {/* <button className={style.button} type="submit">
+            Create Account
+          </button> */}
+          <div className={style.buttonArea}>
+            <button className={`${style.button} ${style.signup}`} role="button">
+              <span class="text">Create Account</span>
+              <span>Click to create account</span>
+            </button>
+            <button class={style.button} role="button">
+              <span class="text">Have an account?</span>
+              <span>Login</span>
+            </button>
+          </div>
         </form>
       </div>
     </main>
--- app/account/testcomponent.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-
-const testcomponent = () => {
-  return <div style={{ width: "100vw" }}>testcomponent</div>;
-};
-
-export default testcomponent;
